aba1337c58163e9ee9db3f0a1778620501345df8
Katelynn Rainey <s78528@WL165T101800.sfps.info>
2022-03-01 11:40:00 -0700

    added searchbar and responsive menu bar

--- code.js
+++ code.js
@@ -24,4 +24,16 @@
     //document.getElementById('output').innerHTML = colorpath;
 };  
 
+function myFunction() {
+    var x = document.getElementById("myTopNav");
+    if(x.className === "topnav")
+    {
+        x.className += " responsive";
+    }
+    else
+    {
+        x.className = "topnav";
+    }
+}
+
   
